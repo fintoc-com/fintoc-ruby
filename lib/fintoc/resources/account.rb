a0226b47afe8a587574455ddd9ef857ffa87b3bc
juanca sardin <juan.carlos.sardin@gmail.com>
2023-07-11 00:12:31 -0400

    Refactor account and link resource to use the resource base class

--- lib/fintoc/resources/account.rb
+++ lib/fintoc/resources/account.rb
@@ -4,51 +4,11 @@ require 'fintoc/resources/movement'
 require 'fintoc/resources/balance'
 
 module Fintoc
-  class Account
+  class Account < Resource
     include Utils
-    attr_reader :id, :name, :holder_name, :currency, :type, :refreshed_at,
-                :official_name, :number, :holder_id, :balance, :movements
-    def initialize(
-      id:,
-      name:,
-      official_name:,
-      number:,
-      holder_id:,
-      holder_name:,
-      type:,
-      currency:,
-      refreshed_at: nil,
-      balance: nil,
-      movements: nil,
-      client: nil,
-      **
-    )
-      @id = id
-      @name = name
-      @official_name = official_name
-      @number = number
-      @holder_id = holder_id
-      @holder_name = holder_name
-      @type = type
-      @currency = currency
-      @refreshed_at = DateTime.iso8601(refreshed_at) if refreshed_at
-      @balance = Fintoc::Balance.new(**balance)
-      @movements = movements || []
-      @client = client
-    end
 
-    def update_balance
-      @balance = Fintoc::Balance.new(**get_account[:balance])
-    end
-
-    def get_movements(**params)
-      _get_movements(**params).lazy.map { |movement| Fintoc::Movement.new(**movement) }
-    end
-
-    def update_movements(**params)
-      @movements += get_movements(**params).to_a
-      @movements = @movements.uniq.sort_by(&:post_date)
-    end
+    belongs_to :balance, class_name: Fintoc::Balance.to_s
+    has_many :movements, class_name: Fintoc::Movement.to_s
 
     def show_movements(rows = 5)
       puts("This account has #{Utils.pluralize(@movements.size, 'movement')}.")
@@ -66,19 +26,6 @@ module Fintoc
 
     def to_s
       "💰 #{@holder_name}’s #{@name} #{@balance}"
-    end
-
-    private
-
-    def get_account
-      @client.get.call("accounts/#{@id}")
-    end
-
-    def _get_movements(**params)
-      first = @client.get.call("accounts/#{@id}/movements", **params)
-      return first if params.empty?
-
-      first + Utils.flatten(@client.fetch_next)
     end
   end
 end
--- lib/fintoc/resources/link.rb
+++ lib/fintoc/resources/link.rb
@@ -1,64 +1,27 @@
 require 'date'
 require 'tabulate'
 require 'fintoc/utils'
-require 'fintoc/resources/account'
-require 'fintoc/resources/institution'
 require 'tabulate'
 
 module Fintoc
-  class Link
-    attr_reader :id, :username, :holder_type, :institution, :created_at, :mode,
-                :accounts, :link_token
+  class Link < Resource
+
+    has_many :accounts, class_name: Account.to_s
+
+    belongs_to :institution, class_name: Institution.to_s
+
     include Utils
-    def initialize(
-      id:,
-      username:,
-      holder_type:,
-      institution:,
-      created_at:,
-      mode:,
-      accounts: nil,
-      link_token: nil,
-      client: nil,
-      **
-    )
-      @id = id
-      @username = username
-      @holder_type = holder_type
-      @institution = Fintoc::Institution.new(**institution)
-      @created_at = Date.iso8601(created_at)
-      @mode = mode
-      @accounts = if accounts.nil?
-                    []
-                  else
-                    accounts.map { |data| Fintoc::Account.new(**data, client: client) }
-                  end
-      @token = link_token
-      @client = client
-    end
-
-    def find_all(**kwargs)
-      raise 'You must provide *exactly one* account field.' if kwargs.size != 1
-
-      field, value = kwargs.to_a.first
-      @accounts.select do |account|
-        account.send(field.to_sym) == value
-      end
-    end
-
-    def find(**kwargs)
-      results = find_all(**kwargs)
-      results.any? ? results.first : nil
-    end
 
     def show_accounts(rows = 5)
       puts "This links has #{Utils.pluralize(@accounts.size, 'account')}"
       return unless @accounts.any?
 
-      accounts = @accounts.to_a.slice(0, rows)
-                          .map.with_index do |acc, index|
-                            [index + 1, acc.name, acc.holder_name, acc.currency]
-                          end
+      accounts = @accounts
+                  .to_a
+                  .slice(0, rows)
+                  .map.with_index do |acc, index|
+                    [index + 1, acc.name, acc.holder_name, acc.currency]
+                  end
       headers = ['#', 'Name', 'Holder', 'Currency']
       puts
       puts tabulate(headers, accounts, indent: 4, style: 'fancy')
