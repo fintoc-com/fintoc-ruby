9f09bf90474dca9fc73dd82545f47448f5104d2c
Pedro Bahamondes <pibahamondesw@gmail.com>
2025-08-28 08:20:53 -0400

    feature(entities): Add entities endpoints

--- spec/lib/fintoc/client_spec.rb
+++ spec/lib/fintoc/client_spec.rb
@@ -45,4 +45,70 @@ RSpec.describe Fintoc::Client do
       end.to output(start_with('This links has 1 account')).to_stdout
     end
   end
+
+  describe '#get_entity' do
+    let(:entity_id) { 'ent_12345' }
+
+    let(:entity_response) do
+      {
+        object: 'entity',
+        mode: 'test',
+        id: entity_id,
+        holder_name: 'Test Company LLC',
+        holder_id: '12345678-9',
+        is_root: true
+      }
+    end
+
+    before do
+      allow(client).to receive(:_get_entity).with(entity_id).and_return(entity_response)
+    end
+
+    it 'returns an Entity instance' do
+      entity = client.get_entity(entity_id)
+
+      expect(entity)
+        .to be_an_instance_of(Fintoc::Entity)
+        .and have_attributes(
+          id: entity_id,
+          holder_name: 'Test Company LLC'
+        )
+    end
+  end
+
+  describe '#get_entities' do
+    let(:entities_reponse) do
+      [
+        {
+          object: 'entity',
+          mode: 'test',
+          id: 'ent_12345',
+          holder_name: 'Test Company LLC',
+          holder_id: '12345678-9',
+          is_root: true
+        },
+        {
+          object: 'entity',
+          mode: 'test',
+          id: 'ent_67890',
+          holder_name: 'Another Company',
+          holder_id: '98765432-1',
+          is_root: false
+        }
+      ]
+    end
+
+    before do
+      allow(client).to receive(:_get_entities).and_return(entities_reponse)
+    end
+
+    it 'returns an array of Entity instances' do
+      entities = client.get_entities
+
+      expect(entities).to all(be_a(Fintoc::Entity))
+      expect(entities.size).to eq(2)
+      expect(entities.first.id).to eq('ent_12345')
+      expect(entities.last.id).to eq('ent_67890')
+    end
+  end
 end
