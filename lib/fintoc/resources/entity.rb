1133e619027dc659a79bd636d3a92585d83f8216
Pedro Bahamondes <pibahamondesw@gmail.com>
2025-08-28 08:28:32 -0400

    feature(entity): Add refresh method to entity

--- lib/fintoc/resources/entity.rb
+++ lib/fintoc/resources/entity.rb
@@ -28,5 +28,12 @@ module Fintoc
     def to_s
       "🏢 #{@holder_name} (#{@id})"
     end
+
+    def refresh
+      entity_data = @client.get_entity(@id)
+      initialize(**entity_data.instance_variables.each_with_object({}) do |var, hash|
+        hash[var.to_s.delete('@').to_sym] = entity_data.instance_variable_get(var)
+      end)
+    end
   end
 end
