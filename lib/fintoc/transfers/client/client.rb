8d4ad50ac68010f2bac4a5c12eab9820675313da
Pedro Bahamondes <pibahamondesw@gmail.com>
2025-08-29 16:06:46 -0400

    feature(accounts): Add accounts methods to Transfers client

--- lib/fintoc/transfers/client/client.rb
+++ lib/fintoc/transfers/client/client.rb
@@ -1,10 +1,12 @@
 require 'fintoc/base_client'
 require 'fintoc/transfers/client/entities_methods'
+require 'fintoc/transfers/client/accounts_methods'
 
 module Fintoc
   module Transfers
     class Client < BaseClient
       include EntitiesMethods
+      include AccountsMethods
     end
   end
 end
