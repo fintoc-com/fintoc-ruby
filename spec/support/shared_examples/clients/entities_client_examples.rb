04bb3a94ae00c7a5e4def375e0eebf05d86eddba
Pedro Bahamondes <pibahamondesw@gmail.com>
2025-08-29 11:25:25 -0400

    refactor(transfers-client): Update spec to mock api response rather than method response

--- spec/support/shared_examples/clients/entities_client_examples.rb
+++ spec/support/shared_examples/clients/entities_client_examples.rb
@@ -1,4 +1,6 @@
 RSpec.shared_examples 'a client with entities methods' do
+  let(:entity_id) { 'ent_31t0VhhrAXASFQTVYfCfIBnljbT' }
+
   it 'responds to entity-specific methods' do
     expect(client)
       .to respond_to(:get_entity)
@@ -6,67 +8,25 @@ RSpec.shared_examples 'a client with entities methods' do
   end
 
   describe '#get_entity' do
-    let(:entity_id) { 'ent_12345' }
-    let(:entity_response) do
-      {
-        object: 'entity',
-        mode: 'test',
-        id: entity_id,
-        holder_name: 'Test Company LLC',
-        holder_id: '12345678-9',
-        is_root: true
-      }
-    end
-
-    before do
-      allow(client).to receive(:_get_entity).with(entity_id).and_return(entity_response)
-    end
-
-    it 'returns an Entity instance' do
+    it 'returns an Entity instance', :vcr do
       entity = client.get_entity(entity_id)
 
       expect(entity)
         .to be_an_instance_of(Fintoc::Transfers::Entity)
         .and have_attributes(
           id: entity_id,
-          holder_name: 'Test Company LLC'
+          holder_name: 'Fintoc'
         )
     end
   end
 
   describe '#get_entities' do
-    let(:entities_response) do
-      [
-        {
-          object: 'entity',
-          mode: 'test',
-          id: 'ent_12345',
-          holder_name: 'Test Company LLC',
-          holder_id: '12345678-9',
-          is_root: true
-        },
-        {
-          object: 'entity',
-          mode: 'test',
-          id: 'ent_67890',
-          holder_name: 'Another Company Inc',
-          holder_id: '98765432-1',
-          is_root: false
-        }
-      ]
-    end
-
-    before do
-      allow(client).to receive(:_get_entities).and_return(entities_response)
-    end
-
-    it 'returns an array of Entity instances' do
+    it 'returns an array of Entity instances', :vcr do
       entities = client.get_entities
 
       expect(entities).to all(be_a(Fintoc::Transfers::Entity))
-      expect(entities.size).to eq(2)
-      expect(entities.first.id).to eq('ent_12345')
-      expect(entities.last.id).to eq('ent_67890')
+      expect(entities.size).to eq(1)
+      expect(entities.first.id).to eq('ent_31t0VhhrAXASFQTVYfCfIBnljbT')
     end
   end
 end
