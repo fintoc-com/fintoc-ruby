10a075a694c9f0a9d62e545c77996d06432ddb24
Pedro Bahamondes <pibahamondesw@gmail.com>
2025-09-01 13:30:17 -0400

    feature(account-numbers): Add account numbers methods

--- lib/fintoc/transfers/client/client.rb
+++ lib/fintoc/transfers/client/client.rb
@@ -1,12 +1,14 @@
 require 'fintoc/base_client'
 require 'fintoc/transfers/client/entities_methods'
 require 'fintoc/transfers/client/accounts_methods'
+require 'fintoc/transfers/client/account_numbers_methods'
 
 module Fintoc
   module Transfers
     class Client < BaseClient
       include EntitiesMethods
       include AccountsMethods
+      include AccountNumbersMethods
     end
   end
 end
